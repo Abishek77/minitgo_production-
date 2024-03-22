7b8bb04806602b57ac308becd75b5bf9456f7c8f
Abishek77 <18e51a0584@hitam.org>
2024-03-22 12:26:58 +0530

    products api

--- api/upload.php
+++ api/upload.php
@@ -4,18 +4,6 @@ $uploadedFile = $uploadDir . basename($_FILES['image']['name']);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($uploadedFile, PATHINFO_EXTENSION));
 
-// Check if image file is a actual image or fake image
-if (isset($_POST['submit'])) {
-    $check = getimagesize($_FILES['image']['tmp_name']);
-    if ($check !== false) {
-        echo 'File is an image - ' . $check['mime'] . '.';
-        $uploadOk = 1;
-    } else {
-        echo 'File is not an image.';
-        $uploadOk = 0;
-    }
-}
-
 // Check if file already exists
 if (file_exists($uploadedFile)) {
     echo 'Sorry, file already exists.';
@@ -41,8 +29,8 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadedFile)) {
-        echo 'The file ' . htmlspecialchars(basename($_FILES['image']['name'])) . ' has been uploaded.';
-        echo 'uploads/' . htmlspecialchars(basename($_FILES['image']['name']));
+        // Return the path to the uploaded file
+        echo $uploadedFile;
     } else {
         echo 'Sorry, there was an error uploading your file.';
     }

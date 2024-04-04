4afe991504121440d7b751bef1283235df6d8a40
Abishek77 <18e51a0584@hitam.org>
2024-04-04 23:57:50 +0530

    orders

--- api/insert_order.php
+++ api/insert_order.php
@@ -52,7 +52,7 @@ $product_description = $data['product_description'] ?? '';
 
 
 // Update data in the database table
-$sql = "INSERT INTO orders (id, product_id, order_id, product_name, quantity, payment_mode, transition_id, payment_status, client_id, client_name, client_cordnates, user_name, user_id, user_cordnates, user_address, product_color, product_price, delivery_boy_name, delivery_boy_id, delivery_boy_cordnates_from, delivery_boy_cordnates_client, delivery_boy_cordnates_user, status_product_client, status_delivery_boy, product_title, status_delivery_user, product_status, status_after_delivery, `return`, reason, product_image, delivery_boy_phonenumber, `date`, `time`, user_phonenumber, product_description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+$sql = "INSERT INTO orders (id, product_id, order_id, product_name, quantity, payment_mode, transition_id, payment_status, client_id, client_name, client_coordinates, user_name, user_id, user_cordnates, user_address, product_color, product_price, delivery_boy_name, delivery_boy_id, delivery_boy_cordnates_from, delivery_boy_cordnates_client, delivery_boy_cordnates_user, status_product_client, status_delivery_boy, product_title, status_delivery_user, product_status, status_after_delivery, `return`, reason, product_image, delivery_boy_phonenumber, `date`, `time`, user_phonenumber, product_description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 $stmt = mysqli_prepare($conn, $sql);
 
 // Bind parameters to the prepared statement
